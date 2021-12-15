4ef0f1194710d38bcd7c381d776d02e46bdcd95b
Chris Menezes <chris@menezes.ca>
2021-12-14 22:02:41 -0500

    Update day9.kt

--- src/main/kotlin/day9.kt
+++ src/main/kotlin/day9.kt
@@ -1,4 +1,46 @@
 import java.io.File
+import kotlin.collections.ArrayDeque
+
+data class Element(val r: Int, val c: Int) {
+    private fun neighbours(): List<Element> {
+        val up = Element(r - 1, c)
+        val down = Element(r + 1, c)
+
+        val left = Element(r, c - 1)
+        val right = Element(r, c + 1)
+
+        return listOf(up, down, left, right)
+    }
+
+    fun reachableNeighbours(rows: Int, cols: Int): List<Element> {
+        return neighbours()
+            .filter { (ri, ci) -> ri in 0 until rows && (ci in 0 until cols) }
+    }
+}
+
+fun getBasins(lowestPoints: List<Element>, mat: List<List<Int>>): List<Set<Element>> {
+    val l = mutableListOf<Set<Element>>()
+    val visited = mutableSetOf<Element>()
+
+    for (p in lowestPoints) {
+        val stack = ArrayDeque<Element>()
+        val basinSet = mutableSetOf<Element>()
+
+        stack.addAll(p.reachableNeighbours(mat.size, mat.first().size).filter { mat[it.r][it.c] != 9 })
+        while (stack.isNotEmpty()) {
+            println(stack)
+            val e = stack.removeLast()
+            basinSet.add(e)
+            visited.add(e)
+            val newNeighbours = e.reachableNeighbours(mat.size, mat.first().size)
+                .filter { mat[it.r][it.c] != 9 }
+                .filter { !visited.contains(it) && !stack.contains(it) }
+            stack.addAll(newNeighbours)
+        }
+        l.add(basinSet)
+    }
+    return l
+}
 
 fun main() {
     val source = File("src/main/resources/day9.txt").readLines()
@@ -8,27 +50,26 @@ fun main() {
     val cols = mat.first().size
     val rows = mat.size
 
-    val lowestPoints = mutableListOf<Pair<Int,Int>>()
+    val lowestPoints = mutableListOf<Element>()
     for (r in 0 until rows) {
         for (c in 0 until cols) {
 
-            val up = Pair(r - 1, c )
-            val down = Pair(r + 1, c )
-
-            val left = Pair(r, c - 1)
-            val right = Pair(r, c + 1)
-            val isLowest = listOf(up, down, left, right)
-                .filter { (ri, ci) -> ri in 0 until rows && (ci in 0 until cols)}
+            val isLowest = Element(r, c)
+                .reachableNeighbours(rows, cols)
                 .map { (ri, ci) -> mat[ri][ci] }
                 .all { it > mat[r][c] }
 
             if (isLowest) {
-                lowestPoints.add(Pair(r,c))
+                lowestPoints.add(Element(r, c))
             }
             mat[r][c]
         }
     }
 
-    println("Part 1: ${lowestPoints.map { (r,c) -> mat[r][c] + 1 }.sum()}")
+    println("Part 1: ${lowestPoints.map { (r, c) -> mat[r][c] + 1 }.sum()}")
+    println("Part 2: ${getBasins(lowestPoints, mat).map { it.size }.sortedDescending().slice(0..2).multiply()}")
+}
 
+fun List<Int>.multiply(): Int {
+    return reduce { acc, i -> acc*i }
 }
